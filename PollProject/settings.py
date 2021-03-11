629af7b46a3e115c0635b7e4577f7e016b37ee0a
Optimusmello <67249066+Optimusmello@users.noreply.github.com>
2021-03-11 06:26:36 +0200

    Update settings.py

--- PollProject/settings.py
+++ PollProject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'PollDb',
         'USER' : 'postgres',
-        'PASSWORD' : '!@amD6450@',
+        'PASSWORD' : '0000',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
