6de3449f76d15382d20eb7a86977d59d10c6c0db
Optimusmello <67249066+Optimusmello@users.noreply.github.com>
2021-03-11 06:25:25 +0200

    Update settings.py

--- PollProject/settings.py
+++ PollProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0-o!dd++6-5#dskmxzk!6b4z@*9%@eu&tno)io_4$hbd%mb!kd'
+SECRET_KEY = #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
