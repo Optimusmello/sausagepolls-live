d8c4029f780a79ef7050e9b88cfc79265e310b11
Hassangad <hassangad299@gmail.com>
2021-03-13 07:01:28 +0200

    Allowed_Host Changes

--- PollProject/settings.py
+++ PollProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0-o!dd++6-5#dskmxzk!6b4z@*9%@eu&tno)io_4$hbd%mb!kd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sausagepolls.herokuapp.com','127.0.0.1']
 
 
 # Application definition
